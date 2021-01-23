4c1a969b505682bf46b689647aa386d4f8347d0f
Wen <changyaowen19980629@gmail.com>
2021-01-24 02:35:11 +0800

    20210123

--- userRelated/Main.php
+++ userRelated/Main.php
@@ -32,6 +32,15 @@
                 width: auto;
                 border: none;
                 box-sizing: inherit;
+                float: left;
+                padding: 12px 24px;
+            }
+            .user-search {
+                color: white;
+                font-size: 18px;
+                width: auto;
+                border: none;
+                float: left;
                 padding: 12px 24px;
             }
             .notification {
@@ -39,6 +48,7 @@
                 font-size: 18px;
                 width: auto;
                 border: none;
+                float: left;
                 padding: 12px 24px;
             }
 
@@ -50,13 +60,48 @@
                 float: right;
                 padding: 12px 24px;
             }
+            .page-container {
+                max-width: 1400px;
+                margin-top: 80px;
+                padding: 0.01em 16px;
+                box-sizing: inherit;
+                margin-left: auto;
+                margin-right: auto;
+
+
+
+            }
+            .left-col {
+                width: 25%;
+                float: left;
+                box-sizing: inherit;
+                padding: 0.01em 16px;
+
+            }
+            .profile {
+                box-shadow: 0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12);
+                color: #000;
+                background-color: #fff;
+                border-radius: 4px;
+                box-sizing: inherit;
+
+            }
         </style>
     <head>
     <body>
         <div class="top-side">
             <a class="nav-bar"><i class="fa fa-bars"> </i></a>
+            <a class="user-search"><i class="fa fa-search"> </i></a>
             <a class="notification" href="#"><i class="fa fa-bell-o"></i></a>
             <a class="log-out" href="#"><i class="fa fa-power-off"></i></a>
+        </div>
+        <div class="page-container">
+            <div class="left-col">
+                <div class="profile">
+                    <img class="user-img" src="#" style="height: 106px;width: 106px"></img>    
+                    <p><i class></i></p>
+                </div>
+            </div>
         </div>
     </body>
 </html>
