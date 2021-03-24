5767905430e75a8c23071deff1dffaa346b71102
Wen <changyaowen19980629@gmail.com>
2021-03-24 15:19:52 +0800

    20210324

--- partyRelated/PartyPage.php
+++ partyRelated/PartyPage.php
@@ -15,14 +15,16 @@
                 box-sizing: border-box;
 
             }
+            body {
 
+            }    
             .top-side {
                 top: 0;
                 position: fixed;
                 width: 100%;
                 background-color: #000;
                 box-sizing: inherit;
-                font-size: 15px;
+                font-size: inherit;
             }
             .nav-bar {
                 color: white;
@@ -50,8 +52,7 @@
                 padding: 12px 24px;
             }
             .party-create-btn {
-                color: white;
-                font-size: 20px;
+                font-size: 18px;
                 width: auto;
                 border: none;
                 float: left;
@@ -74,6 +75,10 @@
             .container {
                 padding: 16px;
             }
+            .close {
+
+
+            }
             .party-info {
                 background-color: #fefefe;
                 margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
@@ -90,42 +95,37 @@
                 padding: 12px 24px;
             }
             .party-block {
-                margin: 50px 0px 0px 0px;
-                size: 70%;
+                border-style: solid;
+                border: 20px;
+                margin: 100px;
+                padding: 30px 10px 30px 10px;
+                width 500px;
                 text-align: center;
                 border-radius: 4px;
+                font-size: inherit;
                 box-shadow: 0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12);
                 
             }
-            .holder-block {
+            .description-block {
+                border-style: solid;
+                border: 10px;
+                font-family: monospace;
+                font-size: 30px;
+                margin: 20px 50px;
+                padding: 20px 50px;
 
             }
             .members-block {
-                
+                border: solid;
+                border: 10px;
+                margin: 10px;
+                padding: 20px;
+                font-size: inherit;
 
             }
             .per-member-info {
-
-
-            }
-            .avatar {
-
-
-            }
-            .username {
-
-            }
-            .city {
-
-            }
-            .age {
-
-            }
-            .gender {
-
-            }
-            .job {
-
+                margin: 10px;
+                font-size: inherit;
 
             }
         </style>
@@ -136,28 +136,29 @@
             <a class="nav-bar"><i class="fa fa-bars"> </i></a>
             <a class="user-search"><i class="fa fa-search"> </i></a>
             <a class="notification" href="#"><i class="fa fa-bell-o"></i></a>
-            <button class="party-create-btn" onclick="document.getElementById('id01').style.display='block'"><i class="fa fa-plus-square-o"></i></button>
-            <div id="id01" class="party-create-block">
+            <button class="party-create-btn" onclick="document.getElementById('popup-window').style.display='block'"><i class="fa fa-plus-square-o"></i></button>
+            <div id="popup-window" class="party-create-block">
 
                 <form class="party-info animate" action="../partyRelated/CreateParty.php" method="post">
                     <div class="container">
+                        <span class="close" onclick="document.getElementById('popup-window').style.display = 'none'">X</span>
                         <label for="party-name"><b>Party Name</b></label>
-                        <input type="text" placeholder="Enter party name" name="partyName" required>
+                        <input type="text" placeholder="Enter party name" name="partyName" required><br>
 
                         <label for="party-type"><b>Party Type</b></label>
-                        <input type="text" placeholder="Enter party type" name="partyType" required>
+                        <input type="text" placeholder="Enter party type" name="partyType" required><br>
 
                         <label for="loc"><b>Location</b></label>
-                        <input type="text" placeholder="Enter location" name="loc" required>
+                        <input type="text" placeholder="Enter location" name="loc" required><br>
 
                         <label for="party-time"><b>Party Time</b></label>
-                        <input type="date" placeholder="choose party hold time" name="partyTime" required>
+                        <input type="date" placeholder="choose party hold time" name="partyTime" required><br>
 
                         <label for="limit-mem"><b>Limit Member</b></label>
-                        <input type="number" placeholder="Enter utmost member number" name="limitMem" required>
+                        <input type="number" placeholder="Enter utmost member number" name="limitMem" required><br>
 
                         <label for="description"><b>Description</b></label>
-                        <input type="text" placeholder="Descript the party" name="description" required>
+                        <input type="text" placeholder="Descript the party" name="description" required><br>
 
                         <button type="submit">Create</button>
                     </div>
@@ -167,46 +168,43 @@
             <a class="log-out" href="#"><i class="fa fa-power-off"></i></a>
         </div>
         <div class="party-block">
-            <?php
-                echo "dassda";
-            ?>
-            <div class="holder-block">
+            <div class="description-block">
                 <?php
-                     
+                    echo $data[0]->Description;     
                 ?>                    
             </div>
             <div class="members-block">
                 
                 <?php
-               /* 
-                    for($data as $d) {
+
+
+                    foreach($data as $d) {
                         echo '
                             <div class="per-member-info">
                                 <div class="avatar">
-                                    '.echo. '<img src="data:image/jpeg;base64,'.base64_encode( $d->Avatar ).'"/>'.;.' 
                                 </div>
                                 <div class="username">
                                     '.$d->Username.'
                                 </div>
                                 <div class="city">
-                                    '.$d->City.'
+                                    CITY: '.$d->City.'
                                 </div>
                                 <div class="age">
-                                    '.$d->Age.'
+                                    AGE: '.$d->Age.'
                                 </div>
                                 <div class="gender">
-                                    '.$d->Gender.'
+                                    GENDER: '.$d->Gender.'
                                 </div>
                                 <div class="job">
-                                    '.$d->Job.'
+                                    JOB: '.$d->Job.'
                                 </div>
 
                             </div>
                 
-                        '
+                        ';
 
 
-                    }*/
+                    }
                 ?>
             </div>
         </div>
